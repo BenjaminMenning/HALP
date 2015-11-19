59949180edb8ff350a651816dcd51a4d8c53736f
John <John@146.57.194.193>
2015-11-19 10:18:26 -0600

    created the connection object code

--- src/HALP/Connection.java
+++ src/HALP/Connection.java
@@ -1,18 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HALP;
 
 /**
  *
  * @author Ben
  */
-public class Connection implements ConnectionInterface
-{
+
+
     // Will hold IP address, port number for sure
     // Will also possibly hold connection ID, fileName, and data rate
     // Include setters, getters, and print methods at minimum
+public class Connection implements ConnectionInterface
+{
+ String ipClient;
+ int portClient;
+ String ipServer;
+ int portServer;
+ int rate;
+
+    public Connection(String ipClient, int portClient, String ipServer, int portServer, int rate) {
+        this.ipClient = ipClient;
+        this.portClient = portClient;
+        this.ipServer = ipServer;
+        this.portServer = portServer;
+        this.rate = rate;
+    }
+
+    public String getIpClient() {
+        return ipClient;
+    }
+
+    public void setIpClient(String ipClient) {
+        this.ipClient = ipClient;
+    }
+
+    public int getPortClient() {
+        return portClient;
+    }
+
+    public void setPortClient(int portClient) {
+        this.portClient = portClient;
+    }
+
+    public String getIpServer() {
+        return ipServer;
+    }
+
+    public void setIpServer(String ipServer) {
+        this.ipServer = ipServer;
+    }
+
+    public int getPortServer() {
+        return portServer;
+    }
+
+    public void setPortServer(int portServer) {
+        this.portServer = portServer;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Connection{" + "ipClient=" + ipClient + ", portClient=" + portClient + ", ipServer=" + ipServer + ", portServer=" + portServer + ", rate=" + rate + '}';
+    }
+    
+    
 
 }
